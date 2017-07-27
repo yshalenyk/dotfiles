ce267fa022c7a749c035ac758b0e28145875110b
Yaroslav Shalenyk <yshalenyk@gmail.com>
2017-07-28 00:45:40 +0300

    Extend bootstrap script

--- install.py
+++ install.py
@@ -1,24 +1,40 @@
 import os
+import subprocess
+from functools import partial
 import logging
 
-
+os.system = partial(subprocess.call, shell=True)
 logging.basicConfig(level=logging.INFO)
+FEDORA = '25'
+PYTHONS = [
+    '2.7.13',
+    '3.6.2rc2',
+    'pypy2.7-5.8.0',
+    'pypy3.5-5.8.0'
+]
 ADDITIONAL_PKGS = [
     'https://releases.hashicorp.com/vagrant/1.9.5/vagrant_1.9.5_x86_64.rpm',
     'https://download.cdn.viber.com/desktop/Linux/viber.rpm'
 ]
 INSTALL = [
+    'openssl-devel',
+    'xz',
+    'xz-devel',
+    'https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-{}.noarch.rpm'.format(FEDORA),
     'adobe-source-sans-pro-fonts',
     'adobe-source-serif-pro-fonts',
     'adobe-source-code-pro-fonts',
     'python-devel',
     'python3-devel',
+    'nodejs',
     'automake',
     'gcc',
     'gcc-c++',
     'kernel-devel',
     'cmake',
     'wget',
+    'libffi',
+    'libffi-devel',
     'vim',
     'ctags',
     'cmake',
@@ -32,13 +48,14 @@ INSTALL = [
     'bzip2-devel',
     'readline-devel',
     'sqlite',
-    'sqlite-devel',
+    'feh',
     'sqlite-devel',
     'vlc',
     'htop',
     'xclip',
     'curl',
-    'joe'
+    'joe',
+    'jq'
 ]
 REMOVE = [
     'korganizer',
@@ -51,6 +68,7 @@ REMOVE = [
     'konqueror',
     'ktp-*',
     'qupzilla',
+    'kaddressbook-*'
 ]
 CMD = 'sudo dnf {} {}'
 
@@ -73,9 +91,17 @@ def install_pyenv():
 def install_additional_pkgs():
     """TODO: """
 
-    
+
+def install_pythons():
+    os.system('pyenv update')
+    for python in PYTHONS:
+        os.system('pyenv install {}'.format(python))
+
+
 if __name__ == '__main__':
     install_packages()
     remove_packages()
     install_pyenv()
-    
+    for pks in ADDITIONAL_PKGS:
+        os.system(CMD.format('install -y', pks))
+    install_pythons()
