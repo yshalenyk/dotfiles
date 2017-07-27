0904302fb088dbdd4deba441412b417480d2614b
Yaroslav Shalenyk <yshalenyk@gmail.com>
2017-07-28 00:44:59 +0300

    Added gitconfig

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ vimrcfile="$HOME/.vimrc"
 tmuxrcfile="$HOME/.tmux.conf"
 neovimrc="$HOME/.config/nvim/init.vim"
 ctagsrc="$HOME/.ctags"
+gitconfig="$HOME/.gitconfig"
 
 for file in $vimrcfile $tmuxrcfile $neovimrc $ctagsrc; do
 	if [ -L $file ] || [ -e $file ]; then
@@ -15,3 +16,4 @@ ln -s $(pwd)/vimrc $vimrcfile
 ln -s $(pwd)/tmux.conf $tmuxrcfile
 ln -s $(pwd)/vimrc $neovimrc
 ln -s $(pwd)/ctags $ctagsrc
+ln -s $(pwd)/gitconfig $gitconfig
