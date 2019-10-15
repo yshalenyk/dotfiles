950699825bb2ca194f1b152e966bb5afbee337d3
Yaroslav Shalenyk <yshalenyk@gmail.com>
2019-10-15 18:58:05 +0300

    Switch to ivy

--- init.el
+++ init.el
@@ -13,23 +13,26 @@
 (setq ring-bell-function 'ignore )	; silent bell when you make a mistake
 (setq coding-system-for-read 'utf-8 )	; use utf-8 by default
 (setq coding-system-for-write 'utf-8 )
-(set-language-environment "UTF-8")
-(set-default-coding-systems 'utf-8)
 (setq sentence-end-double-space nil)	; sentence SHOULD end with only a point.
 ;; (setq-default default-fill-column 80)	; toggle wrapping text at the 80th character
-(setq initial-scratch-message
-      (format "Welcome home, %s :)" (user-login-name))) ; print a default message in the empty scratch buffer opened at startup
+(setq initial-scratch-message "Welcome in Emacs") ; print a default message in the empty scratch buffer opened at startup
 (defalias 'yes-or-no-p 'y-or-n-p)       ; must have
 (setq-default truncate-lines t)         ; straight lines
 (global-hl-line-mode 1)                 ; highlight current line
 ;; (setq default-frame-alist '((font . "Source Code Pro-12")))
 ;; (set-frame-font "Source Code Pro-12")   ; set default font
-(set-frame-font "Noto Mono-12" t t)   ; set default font
-(setq default-frame-alist '((font . "Noto Mono-12")))
+;; (set-frame-font "Noto Mono-10.5" t t)   ; set default font
+;; (setq default-frame-alist '((font . "Noto Mono-10.5")))
+(set-frame-font "Source Code Pro Medium-11" t t)   ; set default font
+(setq default-frame-alist '((font . "Source Code Pro Medium-11")))
+
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+;; (setq-default package-check-signature nil)
 
 
 ;;; Apperiance settings (minimal mode)
-(menu-bar-mode -1)
+;;(menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
@@ -39,6 +42,7 @@
 (show-paren-mode 1)
 (delete-selection-mode 1)
 (setq-default indent-tabs-mode -1)
+(setq-default tramp-default-method "ssh")
 
 
 ;;; disable init.el modifying by custom system
@@ -49,11 +53,11 @@
 (load custom-file)
 
 ;;; packaging
-
+(setq-default gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ;; bug in emacs-26.2
 (require 'package)
 (setq package-enable-at-startup nil) ; tells emacs not to load any packages before starting up
 (setq package-check-signature nil)   ; allow unsigned
- 
+
 
 ;;; the following lines tell emacs where on the internet to look up
 ;;; for new packages.
@@ -92,97 +96,19 @@
 (use-package monitor
   :ensure t)
 
-;;; better org
-(use-package org-bullets
-  :ensure t
-  :hook (org-mode . org-bullets-mode))
-
-;;; Setup evil
-
-;;; basic package
-(use-package evil
-  :ensure t
-  :init (setq evil-want-keybinding nil)
-  :config (evil-mode t))
-(use-package org-evil
-  :ensure t)
-(use-package evil-leader
-  :ensure t
-  :config (progn
-	    (evil-leader/set-leader "C-p")
-	    (global-evil-leader-mode)))
-
-
-;;; redefine keybindings
-(use-package evil-collection
-  :after evil
-  :ensure t
-  :custom (evil-collection-setup-minibuffer t)
-  :config (evil-collection-init))
-
-;;; must have stuff from vim
-(use-package evil-surround
-  :ensure t
-  :config
-  (global-evil-surround-mode 1))
-
-(use-package evil-commentary
-  :ensure t
-  :config
-  (evil-commentary-mode 1))
-
 
 ;;; setup theme
-;; (load-theme 'wombat)
-;;; still can't choose propper theme for myself
-;; (use-package gruvbox-theme
-;;   :ensure t
-;;   :config (load-theme 'gruvbox-light-soft t))
-(use-package nord-theme
+
+(use-package almost-mono-themes
   :ensure t
-  :config (load-theme 'nord t))
-;; (use-package zeno-theme 
-;;   :ensure t
-;;   :config (load-theme 'zeno t))
-;; (use-package material-theme
-;;   :ensure t
-;;   :config (load-theme 'material-light t))
-
-
+  :config (load-theme 'almost-mono-white t))
+;; (use-package nord-theme
+;;  :ensure t
+;;  :config (load-theme 'nord t))
 
 ;; (use-package busybee-theme
 ;;   :ensure t
 ;;   :config (load-theme 'busybee t))
-;; (use-package gruvbox-theme
-;;   :ensure t
-;;   :config (load-theme 'gruvbox-light-soft t))
-;; (use-package base16-theme
-;;   :ensure t 
-;;   :config
-;;   (load-theme 'base16-zenburn t))
-;; (use-package kaolin-themes
-;;   :ensure t
-;;   :config
-;;   (load-theme 'kaolin-dark t))
-
-;; (use-package spacemacs-theme
-;;   :ensure t
-;;   :defer t
-;;   :init
-;;   (load-theme 'spacemacs-dark t))
-;; (use-package tao-theme
-;;   :ensure t
-;;   :defer t
-;;   :init (load-theme 'tao-yang  t))
-;; (use-package anti-zenburn-theme
-;;   :ensure t
-;;   :defer t
-;;   :init (load-theme 'anti-zenburn t))
-;; (use-package  brilliance-dull-theme
-;;   :ensure t
-;;   :defer t
-;;   :init (load-theme 'brilliance-dull t))
-
 
 ;; (use-package gotham-theme
 ;;   :ensure t
@@ -203,33 +129,10 @@
   :config (golden-ratio-mode t))
 
 
-;;; quick jump between windows 
-(use-package ace-window 
+;;; quick jump between windows
+(use-package ace-window
   :ensure t
-  :config (global-set-key (kbd "C-x o") 'ace-window))
-
-
-;;; bottom line
-;; (use-package smart-mode-line
-;;   :ensure t
-;;   :init (setq sml/no-confirm-load-theme t)
-;;   :config (progn
-;; 	    (sml/setup)
-;; 	    (setq sml/theme 'respectful)))
-(use-package doom-modeline
-      :ensure t
-      :hook (after-init . doom-modeline-mode)
-      :config (setq
-	       doom-modeline-height 15
-	       doom-modeline-icon t
-	       doom-modeline-major-mode-icon t
-	       doom-modeline-minor-modes t))
-
-;;; line numbers
-(global-linum-mode)
-;; (use-package nlinum 
-;;   :ensure t
-;;   :config (global-nlinum-mode))
+  :config (global-set-key (kbd "C-x C-o") 'ace-window))
 
 
 ;;; more colors
@@ -242,11 +145,18 @@
   :ensure t
   :hook (prog-mode . flycheck-mode))
 
-(use-package flycheck-elm
+(use-package expand-region
   :ensure t
-  :after flycheck
-  :config (add-hook 'flycheck-mode-hook #'flycheck-elm-setup))
+  :config
+  (global-set-key (kbd "C-=") 'er/expand-region)
+  (global-set-key (kbd "C--") 'er/contract-region))
 
+(use-package multiple-cursors
+  :ensure t
+  :config
+  (global-set-key (kbd "C-c C-c m") 'mc/edit-lines)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
 
 ;;; additional languages
 (use-package rust-mode
@@ -263,12 +173,6 @@
 
 (use-package nginx-mode
   :ensure t)
-
-(use-package elm-mode
-  :ensure t
-  :after (company)
-  :config (push 'company-elm company-backends))
-
 
 ;;; autocomplete
 (use-package company
@@ -300,7 +204,6 @@
   :after (lsp-mode)
   :config (lsp-ui-mode))
 
-
 (use-package company-lsp
   :ensure t
   :after (company lps-mode)
@@ -311,6 +214,10 @@
   :after (company)
   :config (push 'company-ansible company-backends))
 
+(use-package company-terraform
+  :ensure t
+  :after company
+  :config (company-terraform-init))
 
 ;;; debugger
 (use-package dap-mode
@@ -329,87 +236,64 @@
   :ensure t
   :config (xclip-mode 1))
 
-
 ;;; better code folding
 (use-package yafolding
   :ensure t
-  :config (progn
-	    (define-key evil-normal-state-map (kbd "SPC") 'yafolding-toggle-element)))
+  :bind ("C-c <C-return>" . yafolding-toggle-element)
+  :hook (prog-mode . yafolding-mode))
 
 ;;; fuzzy finder
-(use-package helm
-  :ensure t
-  :init (progn
-	  (global-set-key (kbd "M-x") 'helm-M-x)
-	  (global-set-key (kbd "C-x C-f") 'helm-find-files)
-	  (global-set-key (kbd "C-x C-b") 'helm-mini)
-	  (setq-default helm-M-x-fuzzy-match t)
-	  (require 'helm-config)
-	  (evil-leader/set-key
-	    "b" 'helm-buffers-list
-	    "y" 'helm-show-kill-ring
-	    "r" 'helm-register
-	    "o" 'helm-occur
-	    "T" 'helm-top
-	    "f" 'helm-find-files))
+(use-package ivy
+  :demand
   :config (progn
-	    (helm-mode)
-	    (helm-autoresize-mode t)))
+	  (ivy-mode t)
+  	  (setq ivy-use-virtual-buffers t)
+	  (setq ivy-height 15)
+  	  (setq enable-recursive-minibuffers t)
+	  (setq ivy-re-builders-alist
+		'((swiper . ivy--regex-plus)
+		  (t      . ivy--regex-fuzzy)))
+  	  (global-set-key "\C-s" 'swiper)
+  	  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+	  (global-set-key (kbd "C-c s") 'counsel-rg)
+	  (global-set-key (kbd "C-x l") 'counsel-locate)
+  	  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  	  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  	  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+	  (global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
+	  (global-set-key (kbd "M-y") 'counsel-yank-pop)
+  	  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  	  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)))
+
+(use-package counsel
+  :after (ivy)
+  :ensure t
+  :config (counsel-mode t))
+
+(use-package ivy-rich
+  :ensure t
+  :after (ivy counsel)
+  :config (ivy-rich-mode t))
+
+(use-package counsel-projectile
+  :ensure t
+  :after (ivy projectile)
+  :config (progn
+	    (global-set-key (kbd "C-c p p") 'counsel-projectile-switch-project)
+	    (global-set-key (kbd "C-c p f") 'counsel-projectile-find-file)))
 
 (use-package projectile
   :ensure t
-  :config (projectile-mode t))
-
-(defun do-grep()
-  "Perform rg search on project."
-  (interactive)
-  (helm-projectile-rg))
-
-(use-package helm-projectile
-  :ensure t
-  :after projectile
-  :config (progn
-	    (use-package helm-rg
-	      :ensure t)
-	    (evil-leader/set-key
-	      "s" #'do-grep)
-	    (evil-leader/set-key
-	      "d" 'projectile-dired)
-	    (evil-leader/set-key
-	      "i" 'imenu)
-	    (evil-leader/set-key
-	      "C-p" 'helm-projectile-switch-project)
-	    (evil-leader/set-key
-	      "p" 'helm-projectile-find-file)
-	    (evil-leader/set-key
-	      "t" 'helm-)
-
-	    (evil-leader/set-key
-	      "h" 'helm-apropos)
-	    (helm-projectile-on)))
+  :config (progn (projectile-mode t)
+		 (setq projectile-completion-system 'ivy)))
 
 ;;; git frontend
 (use-package magit
+  :bind ("C-c g" . magit)
   :ensure t)
 
-(use-package evil-magit
-  :ensure t
-  :after magit
-  :config (evil-leader/set-key
-	    "m" 'magit))
-
-;;; fun
-
-(use-package twittering-mode
-  :ensure t
-  :config (setq
-	   twittering-icon-mode t
-	   twittering-use-icon-storage t
-	   twittering-display-remaining t))
-
-(set-frame-parameter (selected-frame) 'alpha '(90 . 30))
-(add-to-list 'default-frame-alist '(alpha . (90 . 30)))
-
+;;(set-frame-parameter (selected-frame) 'alpha '(90 . 30))
+;;(add-to-list 'default-frame-alist '(alpha . (90 . 30)))
 ;;; TODO: dap-mode
 (provide 'init)
 ;;; init.el ends here
